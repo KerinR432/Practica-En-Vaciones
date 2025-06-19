c8a92f7b6c6b82d8dd3753b47eac167f30a58e08
Ruvik <kerinaguilera@gmail.com>
2025-06-19 10:08:39 +0200

    Cambios en la laptop
    La laptop saludo a la tablet

--- Java/main.java
+++ Java/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hola y bienvenido Ruvik a tu nuevo proyecto de vaciones de Java");
+        System.out.println("Soy la laptop y he recibido tu mensaje");
     }
 }
